30c38730c87cafcc45acffaf73ec343c161cff8f
Max M Baird <maxbaird.gy@gmail.com>
2024-12-04 19:51:02 +0100

    Added add button.

---
+++ lib/aurora/core/presentation/colors.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+const kAuroraCyan = Color(0xFF47D8D4);
+const kAuroraWhite = Color(0xFFFFFFFF);
---
+++ lib/aurora/core/presentation/widgets/aurora_add_button.dart
@@ -0,0 +1,27 @@
+import 'package:aurora/aurora/core/presentation/colors.dart';
+import 'package:flutter/material.dart';
+
+class AuroraAddButton extends StatelessWidget {
+  const AuroraAddButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {},
+      customBorder: const CircleBorder(),
+      child: Ink(
+        width: 200.0,
+        height: 200.0,
+        decoration: const BoxDecoration(
+          shape: BoxShape.circle,
+          color: kAuroraCyan,
+        ),
+        child: const Icon(
+          Icons.add,
+          size: 128.0,
+          color: kAuroraWhite,
+        ),
+      ),
+    );
+  }
+}
